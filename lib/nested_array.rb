f6105d263120aa580bbf2d397c2e2630686e5634
Daniel Mayle <dmayle012@gmail.com>
2019-12-03 21:55:03 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,7 +29,7 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   
-  
+  new_array2 = [ENNET_HOUSE.sort,ENFIELD_TENNIS_ACADEMY.sort]
 end
 
 def matrix_lookup(matrix, row, column)
